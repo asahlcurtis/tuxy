71e8813713c7c318aed691f944a1bd4fcd8296ba
asahlcurtis <131825058+asahlcurtis@users.noreply.github.com>
2025-03-24 20:01:57 -0700

    Update tuxy.cpp

--- tuxy.cpp
+++ tuxy.cpp
@@ -1,3 +1,4 @@
+// tuxy.cpp
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Shared_Image.H>
@@ -16,12 +17,21 @@ using namespace std;
 
 bool hello = false;
 
-void initload() {
-
-}
+string var1sysinfo;
 
 void joke(Fl_Widget *widet, void*) {
 	fl_alert(Jokes[rand() % (sizeof(Jokes) / sizeof(Jokes[0]))].c_str());
+}
+
+void sysinfo(Fl_Widget *widget, void*) {
+	system("python3 getsysinfo.py");
+	ifstream file("sysinfo.txt");
+	string f;
+	string f2;
+	while (getline (file, f)) {
+		f2.append(f);
+	}
+	fl_alert(f2.c_str());
 }
 
 void on_window_close(Fl_Widget *w, void*) {
@@ -29,9 +39,10 @@ void on_window_close(Fl_Widget *w, void*) {
 }
 
 int main() {
-	Fl_PNG_Image *tux = new Fl_PNG_Image("pictures/tuxy.png");
+	Fl_PNG_Image *tux = new Fl_PNG_Image("pictures/tux.png");
 
     	fl_register_images();                       // initialize image lib
+
 						    
     	Fl_Window *imgwin = new Fl_Window(tux->w(),tux->h(),"Tuxy");                 // make a window
 
@@ -51,9 +62,11 @@ int main() {
 	optwin->callback(on_window_close);
 	imgwin->callback(on_window_close);
 
-	Fl_Button *btn1 = new Fl_Button(0, 0, 75, 25, "Joke");
+	Fl_Button *jkbtn = new Fl_Button(0, 0, 75, 25, "Joke");
+	Fl_Button *sysbtn = new Fl_Button(90, 0, 75, 25, "sys info");
 
-	btn1->callback(joke);
+	jkbtn->callback(joke);
+	sysbtn->callback(sysinfo);
 
 	optwin->end();
 	optwin->show();
@@ -61,5 +74,4 @@ int main() {
 
     	return(Fl::run());
 }
-
 
