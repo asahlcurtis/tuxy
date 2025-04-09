d6daf9c0b87a5dc78670f3c1d7c1cde71262a304
asahlcurtis <131825058+asahlcurtis@users.noreply.github.com>
2025-04-09 15:02:14 -0700

    Update tuxy.cpp for v0.0.1
    Updated the main tuxy.cpp for the current version

--- tuxy.cpp
+++ tuxy.cpp
@@ -1,4 +1,5 @@
 // tuxy.cpp
+// tnoremap <Esc> <C-\><C-n> I use nvim btw
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Shared_Image.H>
@@ -19,11 +20,15 @@ bool hello = false;
 
 string var1sysinfo;
 
+// joke callback for the jokes :D
 void joke(Fl_Widget *widet, void*) {
+	// the fl_alert dialog
 	fl_alert(Jokes[rand() % (sizeof(Jokes) / sizeof(Jokes[0]))].c_str());
 }
 
+// callback for generating/opening the system info text file
 void sysinfo(Fl_Widget *widget, void*) {
+	// running the python file to get the system info
 	system("python3 getsysinfo.py");
 	ifstream file("sysinfo.txt");
 	string f;
@@ -34,39 +39,51 @@ void sysinfo(Fl_Widget *widget, void*) {
 	fl_alert(f2.c_str());
 }
 
+void office(Fl_Widget *widget, void*) {
+	system("./tuxyOffice/portal");
+}
+
+// the window closing callback
 void on_window_close(Fl_Widget *w, void*) {
 	exit(0);
 }
 
 int main() {
+	// loading the png image
 	Fl_PNG_Image *tux = new Fl_PNG_Image("pictures/tux.png");
 
     	fl_register_images();                       // initialize image lib
 
-						    
     	Fl_Window *imgwin = new Fl_Window(tux->w(),tux->h(),"Tuxy");                 // make a window
 
+	// just if by some weird thing happened like a cosmic ray hitting a binary bit in my motherboard or disk that deletes the image and then doesn't get to the repo
 	if (tux->fail()) {
         	fl_alert("Failed to load image");
         	return 1;
 	}
 
     	Fl_Box *box = new Fl_Box(0, 0, tux->w(), tux->h());     // widget that will contain image
-    	box->image(tux);					// attach jpg image to box
+    	box->image(tux);					// the jpg image on box
 
+	// the option window for the program ( the window with all of the function )
 	Fl_Window *optwin = new Fl_Window(500, 250, "Option");
 
 	imgwin->end();
 	optwin->begin();
     	
+	// the callbacks for the windows being closed
 	optwin->callback(on_window_close);
 	imgwin->callback(on_window_close);
 
-	Fl_Button *jkbtn = new Fl_Button(0, 0, 75, 25, "Joke");
+	// the button for the jokes
+	Fl_Button *jkebtn = new Fl_Button(0, 0, 75, 25, "Joke");
 	Fl_Button *sysbtn = new Fl_Button(90, 0, 75, 25, "sys info");
+	Fl_Button *ofcbtn = new Fl_Button(180, 0, 80, 25, "Tuxy Office");
 
-	jkbtn->callback(joke);
+	// the callbackes for the buttons
+	jkebtn->callback(joke);
 	sysbtn->callback(sysinfo);
+	ofcbtn->callback(office);
 
 	optwin->end();
 	optwin->show();
@@ -74,4 +91,3 @@ int main() {
 
     	return(Fl::run());
 }
-
